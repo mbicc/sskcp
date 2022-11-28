3e8c7c470da31a35f3318d6874df15989f3c9ab7
mbicc <68183309+mbicc@users.noreply.github.com>
2022-11-28 11:54:16 +0800

    Update sskcp.sh

--- sskcp.sh
+++ sskcp.sh
@@ -78,41 +78,6 @@ rm -f client_linux_amd64 kcptun-linux-amd64-$VERSION.tar.gz
 chmod a+x server_linux_amd64
 mv -f server_linux_amd64 /usr/bin
 
-cat>/etc/kcp-config.json<<EOF
-{
-  "listen": ":51999",
-  "target": "127.0.0.1:443",
-  "key": "veryfast",
-  "crypt": "aes",
-  "mode": "fast",
-  "mtu": 1350,
-  "sndwnd": 512,
-  "rcvwnd": 512,
-  "datashard": 10,
-  "parityshard": 3,
-  "dscp": 0,
-  "nocomp": true,
-  "quiet": false,
-  "tcp": false,
-  "pprof": false
-}
-EOF
-
-cat>/etc/systemd/system/kcp-server.service<<EOF
-[Unit]
-Description=Kcp server
-After=network.target
-[Service]
-ExecStart=/usr/bin/server_linux_amd64 -c /etc/kcp-config.json
-Restart=always
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reload
-systemctl enable kcp-server
-systemctl restart kcp-server
-
 cat>/etc/kcptun-config.json<<EOF
 {
   "listen": ":69",
@@ -149,5 +114,4 @@ systemctl enable kcptun-server
 systemctl restart kcptun-server
 
 systemctl status shadowsocks-server
-systemctl status kcp-server
 systemctl status kcptun-server
